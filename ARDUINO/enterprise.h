91c2cd6ebd4d7fcd53669e6d147d97b3b588cb30
Patrick <prl22@fsu.edu>
2026-03-09 13:06:44 -0400

    added back missing sendData header

--- ARDUINO/enterprise.h
+++ ARDUINO/enterprise.h
@@ -13,3 +13,4 @@ void serverConnect(const char* server);
 void printWifiData();
 void printCurrentNet();
 void printMacAddress(byte mac[]);
+void sendData(String server, String machineID, String studentID, String action);
